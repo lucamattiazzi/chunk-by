650a8c13a533f3ad6511eb590680348a71c8a5cc
Luca Mattiazzi <l.d.mattiazzi@gmail.com>
2021-01-15 13:27:59 +0100

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,5 @@
 export interface ConditionFn<T = any> {
     (chunk: T[], i: number, array: T[], chunksSize: number): boolean;
 }
-declare type Condition<T = any> = ConditionFn<T> | number;
-export declare function chunkBy<T = any>(array: T[], condition: Condition<T>): T[][];
-export {};
+export declare function chunkBy<T = any>(array: T[], condition: number): T[][];
+export declare function chunkBy<T = any>(array: T[], condition: ConditionFn<T>): T[][];
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.chunkBy = void 0;
 function conditionIsNumber(condition) {
     return typeof condition === 'number';
 }
